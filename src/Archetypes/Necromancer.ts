14fe0169a6576fec35c09eb57b19ab62d88e60fa
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-11 17:15:00 -0300

    feat: necromancer class

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,17 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Necromancer extends Archetype {
+  protected static count = 0;
+  energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    Necromancer.count += 1;
+    this.energyType = 'mana';
+  }
+
+  public static createdArchetypesInstances(): number {
+    return Necromancer.count;
+  }
+}
