4c6f21f73d51df3ad74257bff28a81ebb84e5470
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-12 17:22:32 -0300

    feat:  class PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,19 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  protected _player2: Fighter;
+
+  constructor(player: Fighter, player2: Fighter) {
+    super(player);
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    if (this.player.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this.player.attack(this._player2);
+      this._player2.attack(this.player);
+    }
+    return super.fight();
+  }
+}
