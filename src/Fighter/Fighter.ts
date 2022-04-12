b5adcf4de3d55868619d5c3e892f5775fe0a6405
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-12 16:29:42 -0300

    refactoring:  remove attack method

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -5,8 +5,6 @@ export default interface Fighter extends SimpleFighter {
   defense: number;
   energy?: Energy;
 
-  attack(enemy: Fighter): void;
-
   special(enemy: Fighter): void;
 
   levelUp(): void;
